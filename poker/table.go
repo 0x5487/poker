4eb54446455556c35741be0feb5e9ce73c808ed9
Jason Lee <jawc@hotmail.com>
2018-03-29 17:12:03 +0800

    update

--- poker/table.go
+++ poker/table.go
@@ -2,12 +2,16 @@ package poker
 
 import "time"
 
+type Seat struct {
+	ID     int
+	Player Player
+}
+
 type Table struct {
 	Code      string
 	Dealer    *Dealer
-	Players   []*Player
-	maxPlayer int
-	CreatedAt *time.Time `json:"created_at,omitempty" db:"created_at"`
+	Seats     []*Seat
+	CreatedAt *time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at" db:"updated_at"`
 }
 
@@ -15,11 +19,11 @@ func (tbl *Table) GameInfo() {
 
 }
 
-func (tbl *Table) Stand(player *Player) {
+func (tbl *Table) Stand(seatID int) {
 
 }
 
-func (tbl *Table) Sit(player *Player) {
+func (tbl *Table) Sit(seatID int, player *Player) {
 
 }
 
